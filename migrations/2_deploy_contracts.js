dc2f56ea9551999791a517d34ea2bd684789b474
IlyaPrigogine <prigogine@protonmail.com>
2021-07-09 15:30:48 +0800

    feat(migrations/2_deploy_contracts.js): added new deployment scripts for sampleERC20

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,14 @@
 const ConvertLib = artifacts.require("ConvertLib");
 const MetaCoin = artifacts.require("MetaCoin");
+const ERC20 = artifacts.require("ERC20");
 
-module.exports = function(deployer) {
+module.exports = async function(deployer,network,accounts) {
   deployer.deploy(ConvertLib);
   deployer.link(ConvertLib, MetaCoin);
   deployer.deploy(MetaCoin);
+
+  await deployer.deploy(ERC20,"sample","sample");
+
+  const sampleERC20 = await ERC20.deployed();
+  console.log(`sampleERC20: ${sampleERC20.address}`);
 };

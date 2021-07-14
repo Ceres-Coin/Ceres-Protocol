d212ef19a848b2c395324fdf8fd4e77ef3f92c90
IlyaPrigogine <prigogine@protonmail.com>
2021-07-15 07:18:58 +0800

    test(test/ceres_p2.test.js): [ADDED][TEST SCRIPTS]: 'check instanceCERES.ceres_info.call()[3], it default value is equal to global_collateral_ratio()'

--- test/ceres_P2.test.js
+++ test/ceres_P2.test.js
@@ -257,4 +257,9 @@ contract('contracts/Ceres/Ceres.sol', async (accounts) => {
         const expected_value = parseFloat(await instanceCERES.totalSupply.call());
         expect(parseFloat((await instanceCERES.ceres_info.call())[2])).to.equal(expected_value);
     });
+
+    it ('check instanceCERES.ceres_info.call()[3], it default value is equal to global_collateral_ratio()', async() => {
+        const expected_value = parseFloat(await instanceCERES.global_collateral_ratio.call());
+        expect(parseFloat((await instanceCERES.ceres_info.call())[3])).to.equal(expected_value);
+    });
 });

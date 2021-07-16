833eef292588ad8ed308103264db20b90d8b6b36
IlyaPrigogine <prigogine@protonmail.com>
2021-07-16 15:33:58 +0800

    feat(contracts/css/css.sol): [ADDED][CONTRACTS][FUNC]: 'pool_burn_from()'

--- contracts/CSS/CSS.sol
+++ contracts/CSS/CSS.sol
@@ -72,6 +72,19 @@ contract CEREShares is ERC20Custom, AccessControl {
     function setCERESAddress(address ceres_contract_address) external onlyByOwnerOrGovernance {
         CERES = CEREStable(ceres_contract_address);
     }
+    // TODO: ADD TEST CASES
+    function pool_burn_from(address b_address, uint256 b_amount) external onlyPools {
+        // if(trackingVotes){
+        //     trackVotes(b_address, address(this), uint96(b_amount));
+        //     uint32 srcRepNum = numCheckpoints[address(this)];
+        //     uint96 srcRepOld = srcRepNum > 0 ? checkpoints[address(this)][srcRepNum - 1].votes : 0;
+        //     uint96 srcRepNew = sub96(srcRepOld, uint96(b_amount), "pool_burn_from new votes underflows");
+        //     _writeCheckpoint(address(this), srcRepNum, srcRepOld, srcRepNew); // burn votes
+        // }
+
+        super._burnFrom(b_address, b_amount);
+        emit CSSBurned(b_address, address(this), b_amount);
+    }
     
     /* ========== EVENTS ========== */
 

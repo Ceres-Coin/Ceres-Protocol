46afb124bd92f01e227ef61ae18efed9c44ee628
IlyaPrigogine <prigogine@protonmail.com>
2021-07-18 16:31:17 +0800

    style(test/stake_ceres_weth.test.js): [REFORMATTING][CODE]

--- test/Stake_CERES_WETH.test.js
+++ test/Stake_CERES_WETH.test.js
@@ -201,7 +201,7 @@ contract('contracts/Staking/Variants/Stake_CERES_WETH.sol', async (accounts) => 
         expect(await instanceStakingRewards_CERES_WETH.unlockedStakes.call()).to.equal(EXPECTED_VALUE);
     });
 
-    it ("check Stake_CERES_WETH.greylist(account0 - account7), check its value for account0 - 7 should ALL be FALSE ", async() => {
+    it ('check Stake_CERES_WETH.greylist(account0 - account7), check its value for account0 - 7 should ALL be FALSE ', async() => {
 	    const EXPECTED_VALUE = false;
         expect(await instanceStakingRewards_CERES_WETH.greylist.call(account0)).to.equal((EXPECTED_VALUE));
         expect(await instanceStakingRewards_CERES_WETH.greylist.call(account1)).to.equal((EXPECTED_VALUE));

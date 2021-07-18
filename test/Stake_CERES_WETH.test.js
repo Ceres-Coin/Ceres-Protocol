02f414424945095cb2eb3b7ba95623832e8986d8
IlyaPrigogine <prigogine@protonmail.com>
2021-07-18 16:57:41 +0800

    test(test/stake_ceres_weth.test.js): [ADDED][TEST SCRIPTS]: "check Stake_CERES_WETH.balanceOf(account0 - account7), check its value for account0 - 7 should ALL be 0 "

--- test/Stake_CERES_WETH.test.js
+++ test/Stake_CERES_WETH.test.js
@@ -242,6 +242,18 @@ contract('contracts/Staking/Variants/Stake_CERES_WETH.sol', async (accounts) => 
         expect(parseFloat(await instanceStakingRewards_CERES_WETH.crBoostMultiplier.call())).to.equal(parseFloat(1000000));
     });
 
+    it ("check Stake_CERES_WETH.balanceOf(account0 - account7), check its value for account0 - 7 should ALL be 0 ", async() => {
+	    const EXPECTED_VALUE = new BigNumber(0);
+        expect(parseFloat(await instanceStakingRewards_CERES_WETH.balanceOf.call(account0))).to.equal(parseFloat(EXPECTED_VALUE));
+        expect(parseFloat(await instanceStakingRewards_CERES_WETH.balanceOf.call(account1))).to.equal(parseFloat(EXPECTED_VALUE));
+        expect(parseFloat(await instanceStakingRewards_CERES_WETH.balanceOf.call(account2))).to.equal(parseFloat(EXPECTED_VALUE));
+        expect(parseFloat(await instanceStakingRewards_CERES_WETH.balanceOf.call(account3))).to.equal(parseFloat(EXPECTED_VALUE));
+        expect(parseFloat(await instanceStakingRewards_CERES_WETH.balanceOf.call(account4))).to.equal(parseFloat(EXPECTED_VALUE));
+        expect(parseFloat(await instanceStakingRewards_CERES_WETH.balanceOf.call(account5))).to.equal(parseFloat(EXPECTED_VALUE));
+        expect(parseFloat(await instanceStakingRewards_CERES_WETH.balanceOf.call(account6))).to.equal(parseFloat(EXPECTED_VALUE));
+        expect(parseFloat(await instanceStakingRewards_CERES_WETH.balanceOf.call(account7))).to.equal(parseFloat(EXPECTED_VALUE));
+	});
+
 
 
 

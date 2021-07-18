ef4672fadedf201dbdb601effb6ba78e83062ace
IlyaPrigogine <prigogine@protonmail.com>
2021-07-18 17:07:19 +0800

    style(test/stake_ceres_weth.test.js): [REFORMATTING][CODE]

--- test/Stake_CERES_WETH.test.js
+++ test/Stake_CERES_WETH.test.js
@@ -278,7 +278,7 @@ contract('contracts/Staking/Variants/Stake_CERES_WETH.sol', async (accounts) => 
         expect(parseFloat(await instanceStakingRewards_CERES_WETH.lockedBalanceOf.call(account7))).to.equal(parseFloat(EXPECTED_VALUE));
 	});
 
-    it ('check Stake_CERES_WETH.boostedBalanceOf(account0 - account7), check its value for account0 - 7 should ALL be 0 ', async() => {
+    it ('check Stake_CERES_WETH.boostedBalanceOf(account0 - account7), check its value for account0 - 7 should ALL be 0', async() => {
 	    const EXPECTED_VALUE = new BigNumber(0);
         expect(parseFloat(await instanceStakingRewards_CERES_WETH.boostedBalanceOf.call(account0))).to.equal(parseFloat(EXPECTED_VALUE));
         expect(parseFloat(await instanceStakingRewards_CERES_WETH.boostedBalanceOf.call(account1))).to.equal(parseFloat(EXPECTED_VALUE));

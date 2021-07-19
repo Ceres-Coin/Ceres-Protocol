a517d6f145784d5656abb1a93aeb59d6d8029abb
IlyaPrigogine <prigogine@protonmail.com>
2021-07-19 13:22:47 +0800

    feat(test/ceresdemo.test.js): [CREATED][NEW FILE][TEST SCRIPTS FILE]: 'test/ceresdemo.test.js'

---
+++ test/CeresDemo.test.js
@@ -0,0 +1,66 @@
+const BigNumber = require('bignumber.js');
+const BN = BigNumber.clone({ DECIMAL_PLACES: 9 })
+const chalk = require('chalk');
+const { assert, expect,chai} = require('chai');
+const { expectEvent, send, shouldFail, time, constants, balance} = require('@openzeppelin/test-helpers');
+
+const CEREStable = artifacts.require("Ceres/CEREStable");
+const CEREShares = artifacts.require("CSS/CEREShares");
+const Pool_USDC = artifacts.require("Ceres/Pools/Pool_USDC");
+const UniswapPairOracle_USDC_WETH = artifacts.require("Oracle/Variants/UniswapPairOracle_USDC_WETH");
+const WETH = artifacts.require("ERC20/WETH");
+const FakeCollateral_USDC = artifacts.require("FakeCollateral/FakeCollateral_USDC");
+const StakingRewards_CERES_WETH = artifacts.require("Staking/Variants/Stake_CERES_WETH.sol");
+const UniswapV2Factory = artifacts.require("Uniswap/UniswapV2Factory");
+const ERC20 = artifacts.require("ERC20");
+const ONE_DEC18 = new BigNumber("1e18");
+const ONE_HUNDRED_DEC18 = new BigNumber("100e18");
+const ONE_MILLION_DEC18 = new BigNumber("1000000e18");
+const ONE_HUNDRED_MILLION_DEC18 = new BigNumber("100000000e18");
+const FIVE_MILLION_DEC18 = new BigNumber("5000000e18");
+const BIG6 = new BigNumber("1e6");
+const BIG18 = new BigNumber("1e18");
+
+contract('contracts/Staking/Variants/Stake_CERES_WETH.sol', async (accounts) => {
+    // set the deploy address
+	const account0 = accounts[0];
+	const account1 = accounts[1];
+	const account2 = accounts[2];
+	const account3 = accounts[3];
+    const account4 = accounts[4];
+    const account5 = accounts[5];
+    const account6 = accounts[6];
+    const account7 = accounts[7];
+
+    const OWNER = account0;
+	const ADMIN = account1;
+    const TEST_ACCOUNT = account7;
+
+    let cssInstance;
+    let ceresInstance;
+    let instance_Pool_USDC;
+    let instance_Pool_USDC_collateral_token;
+    let col_instance_USDC;
+    let instanceStakingRewards_CERES_WETH;
+    let pair_addr_CERES_WETH;
+    let wethInstance;
+    let uniswapFactoryInstance;
+    beforeEach(async() => {
+        cssInstance = await CEREShares.deployed();
+        ceresInstance = await CEREStable.deployed();
+        instance_Pool_USDC = await Pool_USDC.deployed();
+        wethInstance = await WETH.deployed();
+
+        uniswapFactoryInstance = await UniswapV2Factory.deployed(); 
+        instance_Pool_USDC_collateral_token = await ERC20.at(await instance_Pool_USDC.collateral_token());
+        instance_Pool_USDC_CERES = await CEREStable.at(await instance_Pool_USDC.CERES());
+        instance_Pool_USDC_CSS = await CEREShares.at(await instance_Pool_USDC.CSS());
+        col_instance_USDC = await FakeCollateral_USDC.deployed(); 
+        instanceStakingRewards_CERES_WETH = await StakingRewards_CERES_WETH.deployed();
+        pair_addr_CERES_WETH = await uniswapFactoryInstance.getPair(ceresInstance.address, wethInstance.address, { from: OWNER });
+    });
+
+    
+
+
+});

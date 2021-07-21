3a98c6ea311ecd66dffa42681e037659300c5fc3
IlyaPrigogine <prigogine@protonmail.com>
2021-07-22 07:04:05 +0800

    feat(contracts/governance/accesscontrol.sol): [ADDED][LICENSE] '// SPDX-License-Identifier: MIT' && [UPDATED][PRAGMA SOLIDITY VERSION TO '^0.6.12']

--- contracts/Governance/AccessControl.sol
+++ contracts/Governance/AccessControl.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
-
-pragma solidity ^0.6.0;
+pragma solidity ^0.6.12;
 
 import "../Utils/EnumerableSet.sol";
 import "../Utils/Address.sol";
--- contracts/Interfaces/IOracle.sol
+++ contracts/Interfaces/IOracle.sol
@@ -1,4 +1,5 @@
-pragma solidity ^0.6.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.12;
 
 interface IOracle {
     function update() external;

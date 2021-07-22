076f337c5d580b2af6d119e9ecbc6e6f2f833425
IlyaPrigogine <prigogine@protonmail.com>
2021-07-22 08:24:54 +0800

    test(test/treasury.test.js): [ADDED][TEST SCRIPTS]: 'check treasuryInstance.share.call(), its DEFAULT value is pair_instance_CERES_WETH.address'

--- test/Treasury.test.js
+++ test/Treasury.test.js
@@ -112,6 +112,11 @@ contract('contracts/Treasury.sol', async (accounts) => {
         expect(await treasuryInstance.bond.call()).to.equal(EXPECTED_VALUE);
     });
 
+    it('check treasuryInstance.share.call(), its DEFAULT value is pair_instance_CERES_WETH.address', async () => {
+        const EXPECTED_VALUE = pair_instance_CERES_WETH.address;
+        expect(await treasuryInstance.share.call()).to.equal(EXPECTED_VALUE);
+    });
+
 
 
     

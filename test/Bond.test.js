47737dd640a7ba9b71a632dee9e022cd19c7a63d
IlyaPrigogine <prigogine@protonmail.com>
2021-07-23 07:14:43 +0800

    test(test/bond.test.js): [ADDED][TEST SCRIPTS]: 'check bondInstance.decimals.call(), its DEFAULT value is "18"'

--- test/Bond.test.js
+++ test/Bond.test.js
@@ -102,5 +102,10 @@ contract('contracts/Treasury.sol', async (accounts) => {
         const EXPECTED_VALUE = "CSB";
         expect(await bondInstance.symbol.call()).to.equal(EXPECTED_VALUE);
     });
+
+    it('check bondInstance.decimals.call(), its DEFAULT value is "18"', async () => {
+        const EXPECTED_VALUE = new BigNumber("18");
+        expect(parseFloat(await bondInstance.decimals.call())).to.equal(parseFloat(EXPECTED_VALUE));
+    });
     
 });

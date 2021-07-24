a099707eabb4aed400cfa9945849b6285ac1e67e
IlyaPrigogine <prigogine@protonmail.com>
2021-07-24 12:19:21 +0800

    style(contracts/erc20/lptokenwrapper.sol): [ADDED][COMMENTS][TEST CASES DONE]

--- contracts/ERC20/LPTokenWrapper.sol
+++ contracts/ERC20/LPTokenWrapper.sol
@@ -8,16 +8,16 @@ import '../ERC20/IERC20.sol';
 contract LPTokenWrapper {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
-
+    // TEST CASES DONE
     IERC20 public lpt;
 
     uint256 private _totalSupply;
     mapping(address => uint256) private _balances;
-
+    // TEST CASES DONE
     function totalSupply() public view returns (uint256) {
         return _totalSupply;
     }
-
+    // TEST CASES DONE
     function balanceOf(address account) public view returns (uint256) {
         return _balances[account];
     }
